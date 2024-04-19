db20540141013deaff5188b7909792e6dc447339
UgohP <paskyugoh@gmail.com>
2024-04-19 23:22:14 +0100

    correction of 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
+#!/usr/bin/python3
 def multiple_returns(sentence):
+    "a function that returns a tuple with the length of a string and its first character"
     if not sentence:
-        sentence = None
-    if sentence:
-        sen_len = len(sentence)
-    else:
-        sen_len = 0
-    return (sen_len, sentence if not sentence else sentence[:1])
+        return (0, None)
+    return (len(sentence), sentence[0])
